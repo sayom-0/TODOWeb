17930f4bdb69be9910ed1f9ff477b1c0a39f06f1
Sayom <54506361+sayom-0@users.noreply.github.com>
2025-04-27 16:44:06 -0400

    Purposefully broke code

--- src/main/java/org/example/todoweb/TODOListRepository.java
+++ src/main/java/org/example/todoweb/TODOListRepository.java
@@ -11,7 +11,7 @@ import java.util.Hashtable;
 import java.util.List;
 
 public class TODOListRepository
-{
+
     private SessionFactory sessionFactory;
     private Dictionary<Integer, Integer> itemIdDictionary;
 
